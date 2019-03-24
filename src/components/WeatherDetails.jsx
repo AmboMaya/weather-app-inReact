2ca18f554a39e20fbcdbe76a32d906158bab9d92
Ice Maya <echaymaya@gmail.com>
2019-03-24 23:56:57 +1300

    created WeatherDetails component which takes props data from app comp

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
 const WeatherDetails = props => (
-  <div>
-    <h4>Weather Details</h4>
-    <p>{props.city}, {props.country}</p>
-    <ul>
-      {props.temperature && <p> Temperature: {props.temperature} <>&#x2103;</></p>}
-      {props.humidity && <p> Humidity: {props.humidity} <>&#37;</></p>}
-      {props.description && <p> Condition: {props.description} </p>}
-      {props.minTemperature && <p>Temp Min: {props.minTemperature} <>&#x2103;</> </p>}
-      {props.maxTemperature && <p>Temp Max: {props.maxTemperature} <>&#x2103;</> </p>}
-      {props.pressure && <p>Pressure: {props.pressure} hPa</p>}
-      {props.error && <p>{props.error}</p>}
-    </ul>
-  </div>
+	<React.Fragment>
+		<div className='weatherDetails'>
+			<h4>Weather Details</h4>
+			<p>{props.city}, {props.country}</p>
+		<table>
+		<thead>
+			<tr >
+			<th>Temp Min</th>
+			<th>Temp Max</th>
+			<th>Humidity</th> 
+			<th>Pressure</th>
+			<th>Condition</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+			<td>{props.minTemperature} <>&#x2103;</></td>
+			<td>{props.maxTemperature} <>&#x2103;</></td>
+			<td>{props.humidity} <>&#37;</></td>
+			<td>{props.pressure} hPa</td>
+			<td>{props.description}</td>
+			</tr>
+		</tbody>
+		</table>
+	</div>
+	</React.Fragment>
 )
 
 export default WeatherDetails
